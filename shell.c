741473e68797f6f911ce0258f3a72e8002fbb8df
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2017-09-01 21:52:15 +0200

    output a single "> " to indicate waiting for input

--- shell.c
+++ shell.c
@@ -1,6 +1,22 @@
 #include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * Performs the infinite loop until we kill the shell
+ */
+void input_loop()
+{
+    int status;
+
+    do {
+        printf("> ");
+        status = 0;
+    } while (status);
+}
 
 int main(int argc, char **arghv)
 {
+    input_loop();
+
     return EXIT_SUCCESS;
 }
