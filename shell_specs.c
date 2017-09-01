bb6aa02224a23cb35a0e34ac829a3d9fce78c5fe
Hannes Kaeufler <hannes.kaeufler@gmail.com>
2017-09-01 22:10:43 +0200

    minor steps towards tdd

--- shell_specs.c
+++ shell_specs.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
+#include "shell_lib.h"
 
+/**
+ * Stupid simple assert_true with epic output
+ */
 void assert_true(int truthy_value, char *message)
 {
     if (truthy_value)
@@ -14,8 +18,8 @@ void assert_true(int truthy_value, char *message)
 
 int main(int argc, char **arghv)
 {
-    assert_true(1, "Something worked");
-    assert_true(0, "Something broke");
+    assert_true(1 == 1, "Something worked");
+    assert_true(0 == 0, "Something broke");
 
     return 0;
 }
